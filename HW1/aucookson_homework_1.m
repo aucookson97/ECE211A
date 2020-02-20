fe64bf1c4631b591b6edc0a1a91b1861e4ddaed8
Aidan Cookson <aidanucookson@gmail.com>
2020-02-20 10:28:18 -0800

    Completed PDF

--- HW1/aucookson_homework_1.m
+++ HW1/aucookson_homework_1.m
@@ -1,3 +1,7 @@
+% Aidan Cookson 
+% ECE211A Homework 1
+% 02/21/2020
+
 % Prepare Ground Truth Image as 'x'
 img_raw = imread('208001.jpg'); % Load Raw Image from Dataset
 rect = [0 145 256 255];
@@ -9,6 +13,7 @@ x = im2double(img_grey);
 kernel_size = 21;
 h = eye(21) ./ kernel_size; % Normalized Identity Matrix
 y_noiseless = conv2(x, h); % Convolve input with kernel (x ** h), zero-padding edges (size 276 x 276)
+conv_size = size(y_noiseless, 1);
 
 % Add Gaussian Noise to Noiseless y
 var = (.01 * std2(x))^2; % Desired Gaussian Variance
@@ -28,14 +33,13 @@ psnr_x_ynoisy = PSNR(x, y_noisy_crop, R);
 [ssim_x_ynoisy, ~] = ssim(y_noisy_crop, x);
 
 % Naive Deconvolution of Noiseless and Noisy Y
-H = fft2(h, 276, 276) + 1e-12;
+H = fft2(h, conv_size, conv_size) + 1e-12;
 Y_noisy = fft2(y_noisy);
 Y_noiseless = fft2(y_noiseless);
 
 rect_crop = [0 0 256 256];
 x_hat_noisy_naive = imcrop(ifft2(Y_noisy ./ H), rect_crop); % F-1(Y / H)
 x_hat_noiseless_naive = imcrop(ifft2(Y_noiseless ./ H), rect_crop);
-
 
 % Calculate PSNR and SSIM of X_Hat Naive Method
 psnr_x_noiseless_naive = PSNR(x, x_hat_noiseless_naive, R);
@@ -44,7 +48,7 @@ psnr_x_noisy_naive = PSNR(x, x_hat_noisy_naive, R);
 [ssim_x_noisy_naive, ~] = ssim(x_hat_noisy_naive, x);
 
 % Wiener Deconvolution of Noiseles and Noisy Y
-X = fft2(x, 276, 276);
+X = fft2(x, conv_size, conv_size);
 psd_x = X .* conj(X);
 psd_h = H .* conj(H);
 x_hat_noiseless_wiener = ifft2((psd_h ./ (psd_h + 1 ./ psd_x)) .* (Y_noiseless ./ H));
@@ -57,13 +61,13 @@ psnr_x_noisy_wiener = PSNR(x, x_hat_noisy_wiener, R);
 [ssim_x_noisy_wiener, ~] = ssim(x_hat_noisy_wiener, x);
 
 % Power Spectral Density Estimation
-X = fftshift(fft2(x, 276, 276));
+X = fftshift(fft2(x, conv_size, conv_size));
 noise = y_noisy - y_noiseless;
 N = fftshift(fft2(noise, 276, 276));
 psd_x = log10(X .* conj(X));
 psd_n = log10(N .* conj(N));
 
-% Prepare Images
+% Prepare 2 Test Images
 img_2 = imread('69015.jpg');
 img_3 = imread('295087.jpg');
 
@@ -84,19 +88,15 @@ psd_x_2 = log10(X_2 .* conj(X_2));
 psd_x_3 = log10(X_3 .* conj(X_3));
 
 % Estimate PSD Proportional to 1/w^2
-[X, Y] = meshgrid(linspace(-137, 138, 276));
+psd_est = [linspace(1, conv_size/2, conv_size/2) linspace(conv_size/2, 1, conv_size/2)] / (conv_size/2);
+psd_est = psd_est'.^2 * psd_est.^2;
 
-%psd_est = (log10(1 ./ (X.^2))) .* (log10(1 ./ (Y.^2)));
-psd_est = (6 + log10(1 ./ (X.^2) + 1 ./ (Y.^2)));
-psd_est(isinf(psd_est)) = 6;
-
-x_hat_wiener_est = ifft2((psd_h ./ (psd_h + 1 ./ psd_est)) .* (Y_noisy ./ H));
+x_hat_wiener_est = abs(ifft2((conj(psd_h) ./ (psd_h + psd_est)) .* (Y_noisy ./ H)));
 x_hat_wiener_est = imcrop(x_hat_wiener_est, rect_crop);
 
-imshow(x_hat_wiener_est);
-%colormap(contrast(psd_x));
-
-
+% Calculate PSNR and SSIM of X_Hat Wiener Estimated
+psnr_x_wiener_est = PSNR(x, x_hat_wiener_est, R);
+[ssim_x_wiener_est, ~] = ssim(x_hat_wiener_est, x);
 
 
 
