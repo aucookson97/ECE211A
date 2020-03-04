Fixed bug in SVD calculation

HW2/aucookson_homework2.py
@@ -7,15 +7,13 @@ ECE211A Homework 2
 
 from PIL import Image, ImageDraw, ImageFont
 from sklearn.linear_model import OrthogonalMatchingPursuit
-#import skimage.metrics as metrics
+import skimage.metrics as metrics
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 import cv2
-import os
 import random
 
-FONT_PATH = 'C:\\Windows\\Fonts'
 IMG_SIZE = 256
 
 # Turn an Image Into a List of Patches
@@ -50,9 +48,6 @@ def save_patch(filename, patches, grid_size=5):
     gs = gridspec.GridSpec(grid_size, grid_size)
     gs.update(wspace=0.025, hspace=0.05)
     
-    #f_a, axarr_a = plt.subplots(grid_size, grid_size)
-
-
     for i in range(grid_size**2):
         ax = plt.subplot(gs[i])
         ax.imshow(patches[indices[i]], cmap='gray')
@@ -76,7 +71,7 @@ def save_patches(y_patches, m_patches, grid_size=5):
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         
-    f_y.savefig('y_patches_2.png')
+    f_y.savefig('y_patches.png')
     
     f_m = plt.figure(figsize = (grid_size,grid_size))
 
@@ -87,8 +82,7 @@ def save_patches(y_patches, m_patches, grid_size=5):
         ax.set_xticklabels([])
         ax.set_yticklabels([])  
     
-    f_m.savefig('m_patches_2.png')
-
+    f_m.savefig('m_patches.png')
 
 # Draw Text onto an Image
 def draw_text(img, invert=False):
@@ -106,31 +100,26 @@ def draw_text(img, invert=False):
 # Create and Save Images to Files
 def prepare_images():
     # Load two Images
-    img1 = cv2.imread('img_1.jpg')
-    img2 = cv2.imread('img_2.jpg')
+    img1 = cv2.imread('img_3.png')
+    img2 = cv2.imread('img_4.png')
     
     # Convert Images to Grayscale
     y_train = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     y_test = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('y_train.png', y_train)
-    cv2.imwrite('y_test.png', y_test)
+    cv2.imwrite('y_train_2.png', y_train)
+    cv2.imwrite('y_test_2.png', y_test)
     
     # Draw Text to Test image
-    y_test_missing = draw_text(Image.open('y_test.png'))
+    y_test_missing = draw_text(Image.open('y_test_2.png'))
     
-    y_test_missing.save('y_test_missing.png')
+    y_test_missing.save('y_test_2_missing.png')
     
-    y_test_missing = cv2.imread('y_test_missing.png')
-    #cv2.imshow('y_test_missing', y_test_missing)
-   # cv2.waitKey(0)
+    y_test_missing = cv2.imread('y_test_2_missing.png')
     
     # Create Text Mask
     m_test_missing = draw_text(Image.new('1', (IMG_SIZE, IMG_SIZE), color=255), invert=True)
     
-    m_test_missing.save('m_test_missing.png')    
-    #m_test_missing = cv2.imread('m_test_missing.png')
-    #cv2.imshow('m_test_missing', m_test_missing)
-    #cv2.waitKey(0)
+    m_test_missing.save('m_test_2_missing.png')    
     
 def calc_metrics(img1, img2):
    print ('PSNR: {}'.format(metrics.peak_signal_noise_ratio(img1, img2)))
@@ -145,67 +134,49 @@ def load_images():
     m_test_missing = cv2.imread('m_test_missing.png', 0)
     return (y_train, y_test, y_test_missing, m_test_missing)
 
+# Learn Dictionary using K_SVD
 def K_SVD(y_train, tol=100, num_iter=100, S=20):
-    patch_size = 8
-    num_patches = (IMG_SIZE // patch_size)**2
-    y_patches_train = np.asarray(extract_patches(y_train)).reshape((num_patches, patch_size**2))
-    A_ksvd = random_dict()
+    A_ksvd = random_dict() # 64 x 512
     num_atoms = A_ksvd.shape[1]
-    y_patches = []
     
-    # FlattenPatches
-    for patch in y_patches_train:
-         y_patches.append(patch.flatten())
+   # Flatten Patches to 64 x 1
+    y_patches = []  
+    for patch in y_train:
+        y_patches.append(patch.flatten())        
+    Y = np.asarray(y_patches).T # 64 x 1024
     
     for i in range(num_iter):
         print ('Iteration {}'.format(i+1))
         
-        # Calculate x
-        x = np.zeros((num_atoms, num_patches))
+        # Calculate coef
         omp = OrthogonalMatchingPursuit(S, fit_intercept=False)
-        # for i, patch in enumerate(y_patches):
-        #     omp.fit(A_ksvd, patch)
-        #     x[:, i] = omp.coef_
-        omp.fit(A_ksvd, np.asarray(y_patches).T)  
-        coef = omp.coef_.T
+        omp.fit(A_ksvd, Y)  
+        coef = omp.coef_.T # 512 x 1024
+        
         # Residual Threshold Termination
-        error = np.linalg.norm(A_ksvd.dot(coef) - np.asarray(y_patches).T)
+        error = np.linalg.norm(A_ksvd.dot(coef) - Y)
         print ('\t Residual Error: {}'.format(error))
         if error <= tol: 
             return (A_ksvd, True)
         
         # Update Dictionary atom-by-atom
         for atom in range(num_atoms):
-            wk = np.nonzero(coef[atom, :])[0]
+            wk = coef[atom, :] != 0
             
             if np.sum(wk) == 0:
                 continue
 
+            # Ignore Row 'atom'
             A_ksvd[:, atom] = 0   
             
-            Ek = y_patches_train.copy().T
-            #for j in range(num_atoms):  
-               # aj = np.expand_dims(A_ksvd[:, j], axis=1) #64, 1
-                #x_tj = np.expand_dims(x[j, :], axis=0) #1, 1024
-                #Ek = Ek - aj.dot(x_tj)
-            Ek = Ek - A_ksvd.dot(coef)
-
-            Ek_ohm = Ek[:, wk]
-            
+            Ek = Y - A_ksvd.dot(coef)
+            Ek = Ek[:, wk]
             # Update A, x with SVD
-            U, D, V = np.linalg.svd(Ek_ohm, full_matrices=True)
+            U, D, V = np.linalg.svd(Ek, full_matrices=False)
             A_ksvd[:, atom] = U[:, 0]
-            coef[atom, wk] = (V[:, 0] * D[0]).T
+            coef[atom, wk] = (V.T[:, 0] * D[0]).T
             
     return (A_ksvd, False)
-    
-# Calculate Residual = ||Y - Ax||    
-def residual_error(y_patches, A, x):
-    error = 0.0
-    for i, patch in enumerate(y_patches):
-        estimate = A.dot(x[:, i])
-        error += np.linalg.norm(patch - estimate)
-    return error
                 
 # Recover Image Patches Using OMP
 def recover_OMP(A, y_patches, m_patches, S=20):
@@ -217,8 +188,7 @@ def recover_OMP(A, y_patches, m_patches, S=20):
     return recombine_patches(patches_recovered)
         
 # Orthogonal Matching Pursuit
-def _OMP_Patch(A, y_patch, m_patch, omp, S=20):
-   
+def _OMP_Patch(A, y_patch, m_patch, omp, S=20):  
     y_patch = y_patch.flatten()
     m_patch = m_patch.flatten().astype(bool)
     y_patch_masked = y_patch[m_patch]
@@ -233,8 +203,26 @@ def random_dict(num_atoms=512, patch_size=8):
         d[:, i] = d[:,i] / np.linalg.norm(d[:,i])
     return d
 
+# Plot 25 Atoms with highest std
+def sort_by_std(A):
+    grid_size = 5
+    std = np.std(A, axis=0)
+    ind = np.argmin(std)
+    f_a = plt.figure(figsize = (grid_size,grid_size))
+    gs = gridspec.GridSpec(grid_size, grid_size)
+    gs.update(wspace=0.025, hspace=0.05)
+
+    for i in range(grid_size**2):
+        ax = plt.subplot(gs[i])
+        ax.imshow((A[:, ind[i]]).reshape((8, 8)), cmap='gray')
+        
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        
+    f_a.savefig('A_ksvd_std.png')
+
 if __name__=='__main__':
-    prepare_images()
+    #prepare_images()
     
     (y_train, y_test, y_test_missing, m_test_missing) = load_images()
     
@@ -242,17 +230,21 @@ if __name__=='__main__':
     y_test = y_test / 255 
     y_test_missing = y_test_missing / 255
     
-    
-    #calc_metrics(y_test, y_test_missing)
+   # calc_metrics(y_test, y_test_missing)
     
     y_patches_missing = extract_patches(y_test_missing)
     y_patches_test = extract_patches(y_test)
+    y_patches_train = extract_patches(y_train)
     m_patches = extract_patches(m_test_missing)
-    A_ksvd, converged = K_SVD(y_train, tol = 1e-3, num_iter=3)
+    A_ksvd, converged = K_SVD(y_patches_train, tol = 1e-3, num_iter=100)
+    sort_by_std(A_ksvd)
     img_recovered = recover_OMP(A_ksvd, y_patches_missing, m_patches)
-    #save_patches(np.asarray(y_patches), np.asarray(m_patches))
-
+    #save_patches(np.asarray(y_patches_missing), np.asarray(m_patches))
     #img_recovered = recover_OMP(random_dict(), y_patches_missing, m_patches)
+    #calc_metrics(y_test, )
+    #calc_metrics(y_test, img_recovered)
+   # cv2.imwrite('y_test_random.png', img_recovered)
+    plt.figure(4)
     plt.imshow(img_recovered, cmap='gray')
     plt.show()
     #cv2.imwrite('omp_result.png',img_recovered)